c0697245665180a4784333fb44ac4a80fdd0a865
Alex <alexander.orlov@loxal.net>
2015-01-20 01:30:25 +0100

    remove “Top Stories” from the sidebar as well

--- chromeExtension/GoogleNewsTopStoriesRemover/src/customization.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2012 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
- */
-
-if (window.location.hostname.match(/news.google.*/i)) {
-    document.getElementById("replaceable-section-headline").style.display = 'none';
-}
